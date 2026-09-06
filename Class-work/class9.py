068b1f4a7ac5e0fa6a072985bd2434c1f6b608b0
Mr - A N A S <mr.anas1003@gmail.com>
2026-09-06 15:44:15 +0500

    feat: included all resources of SMIT AI/DS Course

---
+++ Class-work/class9.py
@@ -0,0 +1,5 @@
+
+userVal = int(input("print you table, so give me you number which you wanna print in the table"))
+
+for i in range(1, 11):
+    print(f"{userVal} x {i} : {userVal * i}")
---
+++ Class-work/readme.md
@@ -0,0 +1,1 @@
+# using anaconda for jupyter code editor 
